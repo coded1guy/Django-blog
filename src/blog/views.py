0218cab0d4a202a36b2666346292c8a9eb6716e8
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-07-06 11:11:35 +0100

    add neccesary starter code

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
 
-# Create your views here.
+from blog.models import Post
+
+
+def about(request):
+    template = loader.get_template("blog/about.html")
+    return HttpResponse(template.render({}, request))
+
+
+def index(request):
+    posts = Post.objects.all()
+    context = {"post_list": posts}
+    return render(request, "blog/post_list.html", context=context)
+
+
+def post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post_detail.html", {"post": post})
